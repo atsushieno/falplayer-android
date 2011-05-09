6dd295b813c38cc59ccc5078839397518c89d912
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2011-05-10 02:55:42 +0900

    Added app icon and README.

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -27,7 +27,7 @@ namespace Falplayer
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int icon = 2130837504;
 			
 			private Drawable()
 			{
